5750222d9f97a06dd64108a0fe0623ec1ee447b7
LeahJKH <Leahjb06.kodehode@gmail.com>
2025-11-20 09:36:06 +0100

    removed backend parts

--- site/arbeidsplassen.py
+++
@@ -1,1 +0,0 @@
-# https://arbeidsplassen.nav.no/stillinger?county=VESTLAND&v=5&municipal=VESTLAND.BERGEN&q=frontend
--- start.py
+++ start.py
@@ -3,26 +3,21 @@ import os
 import sys
 
 print("Welcome to Leah's job scraper!\n1. Manual\n2. Live server")
-choice = input("Choose: ").strip().lower()
+choice = input("Choice?: ").strip().lower()
 
 python_cmd = sys.executable
 script_dir = os.path.dirname(os.path.abspath(__file__))
 frontend_dir = os.path.join(script_dir, "frontend")
 
-if choice.startswith("1"):
-    site = input("Search site: 1. Finn  2. Arbeidsplassen\n").strip().lower()
-    if site.startswith("1"):
-        subprocess.run([python_cmd, "manual/finn.py"])
-    elif site.startswith("2"):
-        subprocess.run([python_cmd, "manual/arbeidsplassen.py"])
-    else:
-        print("Invalid site.")
-elif choice.startswith("2"):
-    subprocess.Popen([python_cmd, "site/api.py"])
-
+if choice.startswith("1" or "manual"):
+    print("not implimented")
+        # TODO connect backend
+elif choice.startswith("2" or "live server"):
+    subprocess.run("npm i", cwd=frontend_dir, shell=True, check=True)
+    #will try too download neccesary files 
     try:
         subprocess.run("npm run dev", cwd=frontend_dir, shell=True, check=True)
     except FileNotFoundError:
-        print("npm not found! Make sure Node.js and npm are installed and added to PATH.")
+        print("npm install didnt work you are missing files")
 else:
     print("Invalid choice.")
